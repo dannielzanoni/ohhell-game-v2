bd6af15c9ce85da57e142bedb7b5cc271f52fbbb
Daniel Zanoni <danielzanoni2001@gmail.com>
2026-07-17 02:20:42 -0300

    Migrate assets to Git LFS

---
+++ src/games/classic/assets/cardAssetRegistry.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:54c869d6f434925d2eeba8a8c1065858b5d3150a1729237e54e06dcef43e4dfe
+size 3326
---
+++ src/shared/assets/fonts/chomsky/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c049e89198bb66eb40c7a2484a29162462c3b491b2613432d8ea95ed45802bd3
+size 3815
